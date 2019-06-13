4751efefd2222fac58ba571143179c00f0bd7a4a
JPDent <jdelaney.ent@outlook.com>
2019-06-12 21:23:41 -0700

    added array psuedocode

---
+++ array.js
@@ -0,0 +1,22 @@
+// Party Arrays
+
+// ARRAY 1 (rsvnArray): Party with Reservation
+
+// var rsvnArray = []
+
+// ARRAY 2 (rsvnWaitList): Party on Wait List for reservation
+// var rsvnWaitList = []
+
+
+// on click, send object into Array 1 (rsvnArray). 
+// if rsvnArray [i] === 4, push rsvn object to Array 2 (rsvnWaitList)
+// for (i = 0; i < 4; i++) {
+// return [i];
+// },
+// if rsvnArray [i] > 4, push object to Array 1 (rsvnArray)
+// return [i]
+// }
+
+
+
+
